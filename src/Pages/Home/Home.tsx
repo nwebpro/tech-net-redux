a182c94f9d1133437af3a436baf2dfeffd658f2e
nwebpro <nwebpro.work@gmail.com>
2023-07-09 21:35:48 +0600

    increment and decrement with redux

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,16 @@
+import { decrement, increment } from "../../Redux/Features/Counter/counterSlice";
+import { RootState } from "../../Redux/store";
+import { useSelector, useDispatch } from 'react-redux'
+
 const Home = () => {
+	const { count } = useSelector((state: RootState) => state.counter)
+	const dispatch = useDispatch()
+
 	return (
-		<div>
-			Home Page
+		<div className="flex gap-5 h-screen items-center justify-center">
+			<button className="bg-purple-500 text-white py-1 px-3 text-lg rounded" onClick={() => dispatch(increment())}>Increment</button>
+			<h1>{ count }</h1>
+			<button className="bg-red-500 text-white py-1 px-3 text-lg rounded" onClick={() => dispatch(decrement())}>Decrement</button>
 		</div>
 	);
 };
